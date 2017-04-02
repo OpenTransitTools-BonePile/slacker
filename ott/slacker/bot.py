980d3f1ce85943c2f215588e87cc4fb7e80e1359
Frank Purcell <fxpurcell@gmail.com>
2017-04-02 11:50:12 -0700

    sentiment addition to typing

--- ott/slacker/bot.py
+++ ott/slacker/bot.py
@@ -2,7 +2,9 @@ from slackclient import SlackClient
 from textblob import TextBlob
 
 from ott.utils import slack_utils
+from ott.utils import nlp_utils
 import security_keys
+
 
 class Bot(object):
     def __init__(self):
@@ -12,28 +14,34 @@ class Bot(object):
         if bot_id:
             slack_utils.connect_to_slack(slack_client, bot_id, self.slack_responder)
 
-
     def make_response(self, user_input):
         """ take raw text as input, process it with TextBlob, then formulate a proper response 
-        :return: proper response to this input string
+            :return: proper response to this input string
         """
         response = "What does '{}' mean?".format(user_input)
         text_blob = TextBlob(user_input)
-        match, i = self.is_keyword(text_blob, 'geo')
+        match, i = nlp_utils.is_keyword(text_blob, 'geo')
         if match:
-            response = "GeoCoding: '{}'!".format(self.strip(text_blob.words, s=i+1))
+            response = "GeoCoding: '{}'!".format(nlp_utils.strip(text_blob.words, s=i+1))
         else:
-            from_match, i = self.is_keyword(text_blob, 'from', n=5)
-            to_match, j = self.is_keyword(text_blob, 'to', s=i+1, n=10)
+            from_match, i = nlp_utils.is_keyword(text_blob, 'from', n=5)
+            to_match, j = nlp_utils.is_keyword(text_blob, 'to', s=i+2, n=10)
             if from_match and to_match:
-                from_text = self.strip(text_blob.words, s=i+1, e=j-1)
-                to_text = self.strip(text_blob.words, s=j+1)
+                from_text = nlp_utils.strip(text_blob.words, s=i+1, e=j-1)
+                to_text = nlp_utils.strip(text_blob.words, s=j+1)
                 response = "TripPlanner F::{}, T::{}".format(from_text, to_text)
             else:
-                match, i = self.is_keyword(text_blob, 'wing')
+                match, i = nlp_utils.is_keyword(text_blob, 'wing')
                 if match:
-                    r = self.strip(text_blob.words, s=i+1)
-                    response = "all your base are belong to us, so... {}".format(r)
+                    r = nlp_utils.strip(text_blob.words, s=i+1)
+                    response = "'{}' - all your base are belong to us, so...".format(r)
+                else:
+                    sentiment = text_blob.sentiment
+                    if sentiment.polarity > 0:
+                        response = "Not sure what '{}' means exactly, but I can tell someone's having a good day.".format(user_input)
+                    elif sentiment.polarity < 0:
+                        response = "That's not nice...forget it, not even going to try and understand what '{}' means.".format(user_input)
+
         return response
 
     def slack_responder(self, slack_client, command, channel):
