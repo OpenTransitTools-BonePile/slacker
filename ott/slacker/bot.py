e6f1dbc8c4d0f9d367fbc9df71467cafd64caa17
Frank Purcell <fxpurcell@gmail.com>
2017-04-02 11:24:08 -0700

    parse geocode and from & to trip planning

--- ott/slacker/bot.py
+++ ott/slacker/bot.py
@@ -12,28 +12,6 @@ class Bot(object):
         if bot_id:
             slack_utils.connect_to_slack(slack_client, bot_id, self.slack_responder)
 
-    @classmethod
-    def is_keyword(cls, text_blob, keyword, n=1, s=0):
-        """ see if keyword is in the first word (or first n words of the blob)
-            :return both t/f for a match, and the index of the word where the match occurred
-        """
-        # import pdb; pdb.set_trace()
-        match = False
-        index = -1
-        if text_blob:
-            for i, word in enumerate(text_blob.words):
-                if i < s:     # start the keyword search at index #i
-                    continue
-                if i >= n:    # don't evaluate more than n words deep
-                    break
-                suggestions = word.spellcheck()
-                for suggestion in suggestions:
-                    if suggestion[1] > 0.8:
-                        if suggestion[0] == keyword.lower():
-                            match = True
-                            index = i
-                            break
-        return match, index
 
     def make_response(self, user_input):
         """ take raw text as input, process it with TextBlob, then formulate a proper response 
@@ -41,10 +19,21 @@ class Bot(object):
         """
         response = "What does '{}' mean?".format(user_input)
         text_blob = TextBlob(user_input)
-        if self.is_keyword(text_blob, 'geo'):
-            response = "GeoCoding: '{}'!".format(user_input)
-        if self.is_keyword(text_blob, 'wing'):
-            response = "all your base are belong to us, so... {}".format(user_input)
+        match, i = self.is_keyword(text_blob, 'geo')
+        if match:
+            response = "GeoCoding: '{}'!".format(self.strip(text_blob.words, s=i+1))
+        else:
+            from_match, i = self.is_keyword(text_blob, 'from', n=5)
+            to_match, j = self.is_keyword(text_blob, 'to', s=i+1, n=10)
+            if from_match and to_match:
+                from_text = self.strip(text_blob.words, s=i+1, e=j-1)
+                to_text = self.strip(text_blob.words, s=j+1)
+                response = "TripPlanner F::{}, T::{}".format(from_text, to_text)
+            else:
+                match, i = self.is_keyword(text_blob, 'wing')
+                if match:
+                    r = self.strip(text_blob.words, s=i+1)
+                    response = "all your base are belong to us, so... {}".format(r)
         return response
 
     def slack_responder(self, slack_client, command, channel):
