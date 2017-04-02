e5ce5f1f49aef8ab0a165a099feec994558d11f1
Frank Purcell <fxpurcell@gmail.com>
2017-04-02 03:15:25 -0700

    initial

---
+++ setup.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'ott.utils',
+    'pyparsing',
+    'psycopg2',
+    ''
+]
+
+extras_require = dict(
+    dev=[],
+)
+
+#
+# eggs that you need if you're running a version of python lower than 2.7
+#
+if sys.version_info[:2] < (2, 7):
+    requires.extend(['argparse>=1.2.1', 'unittest2>=0.5.1'])
+
+setup(
+    name='ott.slacker',
+    version='0.1.0',
+    description='Open Transit Tools - OTT Loader',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    author="Open Transit Tools",
+    author_email="info@opentransittools.org",
+    dependency_links=[
+        'git+https://github.com/OpenTransitTools/utils.git#egg=ott.utils-0.1.0',
+    ],
+    license="Mozilla-derived (http://opentransittools.com)",
+    url='http://opentransittools.com',
+    keywords='ott, otp, gtfs, gtfsdb, data, database, services, transit',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    extras_require=extras_require,
+    tests_require=requires,
+    test_suite="ott.slacker.tests",
+    # find ott | grep py$ | xargs grep "def.main"
+    entry_points="""
+        [console_scripts]
+        start_server = ott.slacker.bot:Bot.start_server
+    """,
+)
