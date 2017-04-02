5b81413d3f4d28a732746855b5936e0e0f532b9b
Frank Purcell <fxpurcell@gmail.com>
2017-04-02 05:35:47 -0700

    refactor out most of slack to a lib

--- ott/slacker/bot.py
+++ ott/slacker/bot.py
@@ -1,12 +1,7 @@
-import os
-import time
 from slackclient import SlackClient
 import security_keys
 
 from ott.utils import slack_utils
-
-
-EXAMPLE_COMMAND = "do"
 
 
 class Bot(object):
@@ -18,8 +13,19 @@ class Bot(object):
         slack_client = SlackClient(security_keys.SLACK_BOT_TOKEN)
         bot_id = slack_utils.get_bot_id(slack_client, security_keys.BOT_NAME)
         if bot_id:
-            at_bot = "<@{}>".format(bot_id)
-            slack_utils.connect_to_slack(slack_client, at_bot)
+            slack_utils.connect_to_slack(slack_client, bot_id, self.slack_responder)
+
+    def slack_responder(self, slack_client, command, channel):
+        """ Receives commands directed at the bot and determines if they are valid commands. 
+            If so, then acts on the commands. If not, returns back an "I don't know" message...
+            
+        """
+        response = "What does '{}' mean?".format(command)
+        if command.startswith('dd '):
+            response = "Sure...write some more code then I can '{}'!".format(command)
+        if "wing" in command or "zero" in command:
+            response = "all your base are belong to us, so... {}".format(command)
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
     @classmethod
     def start_server(cls):
